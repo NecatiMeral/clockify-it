4e5e50ddd7db1ad0cf99825dd3b0c3bffa471ad8
Necati Meral <necati_meral@yahoo.de>
2022-03-13 15:22:51 +0100

    add invalid configuration error code

--- aspnet-core/src/Sg.ClockifyIt.Domain.Shared/ClockfiyDomainErrorCodes.cs
+++ aspnet-core/src/Sg.ClockifyIt.Domain.Shared/ClockfiyDomainErrorCodes.cs
@@ -2,5 +2,6 @@
 
 public static class ClockifyItDomainErrorCodes
 {
+    public const string InvalidConfiguration = "Sg.ClockifyIt:00001";
     /* You can add your business exception error codes here, as constants */
 }
